468ccbec1705fd2ea075ad57106f8e8c6ef78b32
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2017-09-04 23:55:24 +0530

    Add 432a.cpp

---
+++ prob1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+/*
+ID: arun.ga1
+LANG: C++
+TASK: 
+*/
+
+using namespace std;
+
+#define prime 1000000007
+#define DEBUG(x) cout<<'>'<<#x<<':'<<x<<"\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define pb push_back
+#define p1(x) cout<<x<<"\n" 
+#define p2(x,y) cout<<x<<" "<<y<<"\n" 
+#define p3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define i1(x) cin>>x
+#define i2(x,y) cin>>x>>y
+#define i3(x,y,z) cin>>x>>y>>z
+#define elif else if
+
+const int INF = 1<<29;
+typedef long long ll;
+
+/////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	ll n,k,t;
+	cin >> n >> k >> t;
+	if(t<=k)
+		cout << t;
+	else if(t>=n)
+		cout << n+k - t;
+	else
+		cout << k;
+	return 0;
+}
+
