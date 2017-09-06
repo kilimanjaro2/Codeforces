2512c941f8b8efda6744b3df726830efa85d87fb
kilimanjaro2 <kilimanjaro2@users.noreply.github.com>
2017-09-06 23:38:21 +0530

    Add files via upload

---
+++ prob2.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+/*
+ID: arun.ga1
+LANG: C++
+TASK: 
+*/
+
+using namespace std;
+
+#define prime 1000000007
+#define DEBUG(x) cout<<'>'<<#x<<':'<<x<<"\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define pb push_back
+#define p1(x) cout<<x<<"\n" 
+#define p2(x,y) cout<<x<<" "<<y<<"\n" 
+#define p3(x,y,z) cout<<x<<" "<<y<<" "<<z<<"\n" 
+#define i1(x) cin>>x
+#define i2(x,y) cin>>x>>y
+#define i3(x,y,z) cin>>x>>y>>z
+#define elif else if
+
+const int INF = 1<<29;
+typedef long long ll;
+
+/////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	ll n,k;
+	cin >> n >> k;
+	if(k==0)
+	{
+		cout << "0 0";
+		return 0;
+	}
+	if(n==k)
+	{
+		cout << 0 << " " << 0;
+		return 0;
+	}
+	else
+		cout << 1 << " ";
+	ll ans = n/3;
+	if(k<=ans)
+	{
+		cout << k*2 << endl;
+		return 0;
+	}
+	else if(ans%3)
+	{
+		cout << n-k;
+		return 0;
+	}
+	ans++;
+	if(n%3==2)
+	{
+		cout << n-k;
+		return 0;
+	}
+	else
+	{
+		cout << n-k;
+		return 0;
+	}
+	return 0;
+}
+
